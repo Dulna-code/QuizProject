3d9ce7a94ba89b8f721c9ea49ddf32ba698fdcc8
Dulna-code <dulna.perera11@gmail.com>
2026-09-12 15:57:15 +0200

    Implement QuizService
    Read from csv file into Questions List and error handling.

--- src/quiz/Question.java
+++ src/quiz/Question.java
@@ -2,8 +2,13 @@ package quiz;
 
 public class Question {
     private String questionText;
-    private String[] answersOptions;
+    private String[] answerOptions;
     private int correctAnswerIndex;
 
+    public Question(String questionText, String[] answerOptions, int correctAnswerIndex) {
+        this.questionText = questionText;
+        this.answerOptions = answerOptions;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
 
 }
---
+++ src/quiz/QuizService.java
@@ -0,0 +1,44 @@
+package quiz;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QuizService {
+    public List<Question> loadQuestions(String fileName) {
+        List<Question> questions = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = br.readLine()) != null){
+                String[] parts = line.split(";");
+
+                if (parts.length == 6){
+                    String questionText = parts[0];
+                    String[] answers = { parts[1], parts[2], parts[3], parts[4]};
+
+                    int correctAnswerIndex = Integer.parseInt(parts[5]);
+
+                    Question question = new Question(questionText, answers, correctAnswerIndex);
+                    questions.add(question);
+
+                }else{
+                    System.out.println("Please fix the Question/Answer set: " + Arrays.toString(parts));
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found:" +e.getMessage());
+        } catch (IOException e) {
+            System.out.println(("Error in reading the File: ") + e.getMessage());
+        }catch (NumberFormatException e){
+            System.out.println("The index of the answer is not a valid number" + e.getMessage());
+        }
+
+        return questions;
+    }
+}
